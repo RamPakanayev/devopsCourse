375ee500f58734a360369b090ffb6ca4d3a5f186
Ram Pakanayev <rampakanayev@gmail.com>
2023-03-13 10:47:36 +0200

    my commit 4

--- gitEx3/Hello.java
+++ gitEx3/Hello.java
@@ -2,6 +2,8 @@ public class Hello{
   public static void main (String[] args){
     for(int i=1;i<10;i++){
       System.out.println("hello world ..." + i);
+      System.out.println("ram !");
+      System.out.println("i=" + i);
     }
   }
 }
